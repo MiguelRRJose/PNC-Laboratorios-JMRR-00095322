31f0289c9fb6acb2d140d9584039b24288e1a6b2
MiguelRRJose <00095322@uca.edu.sv>
2026-04-20 15:17:35 -0600

    :memo:[Add] Archivo Repositorio PokemonRepository

--- Laboratorio1/labo01/src/main/java/com/example/labo01/repositories/PokemonRepository.java
+++ Laboratorio1/labo01/src/main/java/com/example/labo01/repositories/PokemonRepository.java
@@ -1,4 +1,18 @@
 package com.example.labo01.repositories;
 
+import com.example.labo01.common.ListaPokemon;
+import com.example.labo01.domain.entities.Pokemon;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
 public class PokemonRepository {
+    private final ListaPokemon listaPokemon;
+
+    public List<Pokemon> findAll() {
+        return listaPokemon.getPokemones();
+    }
 }
