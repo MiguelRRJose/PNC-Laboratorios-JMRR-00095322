0c45540ee0d983675c4f404b2ed30531fc3e2992
MiguelRRJose <00095322@uca.edu.sv>
2026-04-20 15:18:28 -0600

    :memo:[Add] Archivo Logica de Negocio PokemonServices

--- Laboratorio1/labo01/src/main/java/com/example/labo01/services/PokemonServices.java
+++ Laboratorio1/labo01/src/main/java/com/example/labo01/services/PokemonServices.java
@@ -1,4 +1,32 @@
 package com.example.labo01.services;
 
+import com.example.labo01.domain.entities.Pokemon;
+import com.example.labo01.repositories.PokemonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class PokemonServices {
+    private final PokemonRepository pokemonRepository;
+
+    public List<Pokemon> filtrarPorTipo(String tipo) {
+        return pokemonRepository.findAll().stream()
+                .filter(p -> p.getTipo().equalsIgnoreCase(tipo))
+                .toList();
+    }
+
+    public List<Pokemon> filtrarPorZona(String zona) {
+        return pokemonRepository.findAll().stream()
+                .filter(p -> p.getZonas().stream().anyMatch(z -> z.equalsIgnoreCase(zona)))
+                .toList();
+    }
+
+    public List<Pokemon> filtrarPorDebilidad(String debilidad) {
+        return pokemonRepository.findAll().stream()
+                .filter(p -> p.getDebilidades().stream().anyMatch(d -> d.equalsIgnoreCase(debilidad)))
+                .toList();
+    }
 }
