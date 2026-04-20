4a7268ca2bedee1767ae63b87cb34f376f4b0b2e
MiguelRRJose <00095322@uca.edu.sv>
2026-04-20 15:20:31 -0600

    :sparkles:[Add] Laboratorio terminado

--- Laboratorio1/labo01/src/main/java/com/example/labo01/Labo01Application.java
+++ Laboratorio1/labo01/src/main/java/com/example/labo01/Labo01Application.java
@@ -1,7 +1,13 @@
 package com.example.labo01;
 
+import com.example.labo01.domain.entities.Pokemon;
+import com.example.labo01.services.PokemonServices;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Labo01Application {
@@ -10,4 +16,27 @@ public class Labo01Application {
         SpringApplication.run(Labo01Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(PokemonServices pokemonServices) {
+        return args -> {
+            System.out.println("=== POKÉDEX DEL PROFESOR OAK ===");
+
+            System.out.println("\n>> Filtro por tipo: Fuego");
+            imprimir(pokemonServices.filtrarPorTipo("Fuego"));
+
+            System.out.println("\n>> Filtro por zona: Bosque");
+            imprimir(pokemonServices.filtrarPorZona("Bosque"));
+
+            System.out.println("\n>> Filtro por debilidad: Agua");
+            imprimir(pokemonServices.filtrarPorDebilidad("Agua"));
+        };
+    }
+
+    private void imprimir(List<Pokemon> lista) {
+        lista.forEach(p ->
+                System.out.println("[PKMN] Nombre: " + p.getNombre() +
+                        " | Tipo: " + p.getTipo() +
+                        " | Debilidades: " + String.join(", ", p.getDebilidades()))
+        );
+    }
 }
