343d09361ffe909ee07163954eff0a5b24729d95
MiguelRRJose <00095322@uca.edu.sv>
2026-05-18 16:14:18 -0600

    :memo:[FEAT]Mapper de specimen

---
+++ Laboratorio3/Labo03/src/main/java/com/example/labo03/mapper/SpecimenMapper.java
@@ -0,0 +1,47 @@
+package com.example.labo03.mapper;
+
+import com.example.labo03.domain.entity.Specimen;
+import com.example.labo03.dto.request.CreateSpecimenRequest;
+import com.example.labo03.dto.request.UpdateSpecimenRequest;
+import com.example.labo03.dto.response.SpecimenResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class SpecimenMapper {
+
+    public Specimen toEntityCreate(CreateSpecimenRequest request) {
+        return Specimen.builder()
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public Specimen toEntityUpdate(UpdateSpecimenRequest request, UUID id) {
+        return Specimen.builder()
+                .id(id)
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public SpecimenResponse toDto(Specimen specimen) {
+        return SpecimenResponse.builder()
+                .id(specimen.getId())
+                .name(specimen.getName())
+                .region(specimen.getRegion())
+                .dangerLevel(specimen.getDangerLevel())
+                .isFriendly(specimen.getIsFriendly())
+                .build();
+    }
+
+    public Page<SpecimenResponse> toDtoPage(Page<Specimen> specimens) {
+        return specimens.map(this::toDto);
+    }
+}
