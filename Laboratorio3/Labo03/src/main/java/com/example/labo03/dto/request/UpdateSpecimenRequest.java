7a559fc9311d69de730a6e9b526de106c656bf70
MiguelRRJose <00095322@uca.edu.sv>
2026-05-18 16:12:26 -0600

    :memo:[FEAT]Logica de los DTO para Specimen

---
+++ Laboratorio3/Labo03/src/main/java/com/example/labo03/dto/request/UpdateSpecimenRequest.java
@@ -0,0 +1,14 @@
+package com.example.labo03.dto.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateSpecimenRequest {
+    private String name;
+    private String region;
+    private Integer dangerLevel;
+    private Boolean isFriendly;
+}
